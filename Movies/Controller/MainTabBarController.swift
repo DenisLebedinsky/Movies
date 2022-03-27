6c55aab51f42b3d0d3b1904a8553b4312e3658f6
denisLebedinsky <den.lebedinsky@gmail.com>
2022-03-27 19:55:08 +0300

    - remove storyboard - setup launch screen

---
+++ Movies/Controller/MainTabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabBarController.swift
+//  Movies
+//
+//  Created by Denis Lebedinsky on 27.03.2022.
+//
+
+import UIKit
+
+class MainTabBarController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+        // Do any additional setup after loading the view.
+    }
+
+
+}
